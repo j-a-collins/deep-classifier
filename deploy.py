caf6a1eb1bd5fbb0432a1d729c9ffcb189cf06d6
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2023-04-22 14:51:03 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,19 +1,33 @@
 """
 This script creates a Gradio app to classify images of cats and dogs.
 
-The app loads a pre-trained Fastai model ('export.pkl') and provides a user interface
+The app loads a simple neural net model ('export.pkl') and provides a user interface
 for users to upload images. The app returns a classification result (cat, dog, or
 "not dog, not cat" if the confidence is below 90%).
 
 The Gradio app also includes custom text describing the model and a link to the
-related blog hosted on GitHub.
+related blog: www.deepest-net.com.
+
+Author: J-A-Collins
 """
 
 import gradio as gr
 from fastbook import *
+import requests
+from fastai.learner import load_learner
+from pathlib import Path
+from io import BytesIO
 
 # Load model
-learn_inf = load_learner('export.pkl')
+# learn_inf = load_learner('export.pkl')
+model_url = "https://filedn.eu/lI9nnolEiqVYFksWq9Mm30u/export.pkl"
+
+
+def download_model(url):
+    response = requests.get(url)
+    model_data = BytesIO(response.content)
+    return model_data
+
 
 def predict(image):
     """
@@ -27,7 +41,7 @@ def predict(image):
         A string containing the classification result.
     """
     res = learn_inf.predict(image)
-    labels = ['cat', 'dog']
+    labels = ["cat", "dog"]
     probabilities = [float(probability) for probability in res[2]]
     max_probability = max(probabilities)
 
@@ -39,13 +53,18 @@ def predict(image):
     return f"{label} ({probability_percentage}% confidence)"
 
 
+model_data = download_model(model_url)
+learn_inf = load_learner(model_data)
+
 # Define Gradio input and output components
-image_input = gr.inputs.Image(shape=(224, 224), label="Upload an image of a cat or a dog")
+image_input = gr.inputs.Image(
+    shape=(224, 224), label="Upload an image of a cat or a dog"
+)
 label_output = gr.outputs.Textbox(label="Prediction")
 
 # Define custom text
 description = """
-This model was built using only 300 images total as the data set. You can read about it on my blog about deep learning and AI safety. It's called 'Deep.' and it's hosted by GitHub here: <a href="https://j-a-collins.github.io/" target="_blank">https://j-a-collins.github.io/</a>
+This model was built using only 300 images total as the data set. You can read about it on my blog about deep learning and AI safety. It's called 'Deepest Net.' Here: <a href="https://www.deepest-net.com/" target="_blank">https://www.deepest-net.com/</a>
 """
 
 # Define the Gradio interface with a title, custom text, and input/output components
