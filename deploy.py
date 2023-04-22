0d91bdc71f09c849050e2975f1749f188f5fb471
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2023-04-22 21:11:41 +0100

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -11,63 +11,35 @@ related blog: www.deepest-net.com.
 Author: J-A-Collins
 """
 
+import json
+import base64
 import gradio as gr
-from fastbook import *
-import requests
-from fastai.learner import load_learner
-from pathlib import Path
 from io import BytesIO
+import requests
 
-# Load model
-# learn_inf = load_learner('export.pkl')
-model_url = "https://filedn.eu/lI9nnolEiqVYFksWq9Mm30u/export.pkl"
-
-
-def download_model(url):
-    response = requests.get(url)
-    model_data = BytesIO(response.content)
-    return model_data
-
+API_URL = "https://yfskn8ycx8.execute-api.eu-north-1.amazonaws.com/prod/gradioapp"
 
 def predict(image):
-    """
-    Classify the input image as 'cat', 'dog', or, if the model
-    confidence is below 90%, 'not dog, not cat'.
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_base64 = base64.b64encode(buffered.getvalue()).decode()
 
-    Parameters:
-        image: An image file to be classified by the model.
+    payload = json.dumps({"image": img_base64})
+    response = requests.post(API_URL, data=payload, headers={"Content-Type": "application/json"})
 
-    Returns:
-        A string containing the classification result.
-    """
-    res = learn_inf.predict(image)
-    labels = ["cat", "dog"]
-    probabilities = [float(probability) for probability in res[2]]
-    max_probability = max(probabilities)
+    if response.status_code == 200:
+        result = response.json().get("result")
+        return result
+    else:
+        return f"Error: {response.status_code} - {response.text}"
 
-    if max_probability < 0.9:
-        return "Not dog, not cat"
-
-    label = labels[probabilities.index(max_probability)]
-    probability_percentage = round(max_probability * 100, 2)
-    return f"{label} ({probability_percentage}% confidence)"
-
-
-model_data = download_model(model_url)
-learn_inf = load_learner(model_data)
-
-# Define Gradio input and output components
-image_input = gr.inputs.Image(
-    shape=(224, 224), label="Upload an image of a cat or a dog"
-)
+image_input = gr.inputs.Image(shape=(224, 224), label="Upload an image of a cat or a dog")
 label_output = gr.outputs.Textbox(label="Prediction")
 
-# Define custom text
 description = """
 This model was built using only 300 images total as the data set. You can read about it on my blog about deep learning and AI safety. It's called 'Deepest Net.' Here: <a href="https://www.deepest-net.com/" target="_blank">https://www.deepest-net.com/</a>
 """
 
-# Define the Gradio interface with a title, custom text, and input/output components
 gr.Interface(
     fn=predict,
     inputs=image_input,
